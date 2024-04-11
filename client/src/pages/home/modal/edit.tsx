c05dca2fc0047e6a1b99faf6f7b9befe7cb73bc8
dphasst17 <phathuu12@gmail.com>
2024-04-11 09:01:33 +0700

    update: add multi modal

---
+++ client/src/pages/home/modal/edit.tsx
@@ -0,0 +1,67 @@
+import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { useContext } from "react";
+import { StateContext } from "../../../context/stateContext";
+import { useForm } from "react-hook-form";
+import { updateUser } from "../../../api/userApi";
+import { GetToken } from "../../../utils/token";
+import { Modals } from "../../../interface/modal";
+
+interface FormValue {
+  name: string,
+  email: string,
+}
+const ModalEdit = ({ isOpen, onOpenChange, setModalName }: Modals) => {
+  const { user,setUser } = useContext(StateContext)
+  const { register, handleSubmit,formState:{errors}} = useForm<FormValue>()
+  const onSubmit = (data: FormValue) => {
+    const checkData = user?.every((u:any) => data.name !== u.name || data.email !== u.email)
+    const token = GetToken()
+    token && checkData && updateUser(token,data)
+    .then(res => {
+      if(res.status === 200){
+        setUser(user.map((u:any) => {
+          return {
+            ...u,
+            name:data.name,
+            email:data.email
+          }
+        }))
+      }
+      alert(res.message)
+    })
+    
+  }
+  return <Modal
+    isOpen={isOpen}
+    onOpenChange={() => { onOpenChange(); setModalName("") }}
+  >
+    <ModalContent>
+      {(onClose) => (
+        <>
+          <ModalHeader className="flex flex-col gap-1">Edit</ModalHeader>
+          <ModalBody>
+            <form className="w-full">
+              <Input {...register('name', { required: true })} type="text" label="Name" className="w-full my-2" radius="sm" defaultValue={user[0]?.name} />
+              <Input {...register('email', {
+                required: true, pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "invalid email address"
+                }
+              })} type="text" label="Email" className="w-full my-2" radius="sm" defaultValue={user[0]?.email} />
+              {errors.email && <p>{errors.email.message}</p>}
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" variant="light" onPress={() => { onClose(); setModalName("") }}>
+              Close
+            </Button>
+            <Button onClick={() => { handleSubmit(onSubmit)() }} color="success" className="text-white font-bold">Update</Button>
+          </ModalFooter>
+        </>
+      )}
+    </ModalContent>
+  </Modal>
+
+}
+
+export default ModalEdit
--- client/src/pages/home/modal/friend.tsx
+++ client/src/pages/home/modal/friend.tsx
@@ -1,26 +1,46 @@
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { Avatar, Button, Code, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { useContext, useEffect, useState } from "react";
+import { StateContext } from "../../../context/stateContext";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { Modals } from "../../../interface/modal";
 
-const ModalFriend = () => {
+const ModalFriend = ({ isOpen, onOpenChange, setModalName }: Modals) => {
+  const { friend, setChatId, setCurrentUserChat } = useContext(StateContext)
+  const [dataFriend,setDataFriend] = useState<any[] | null>(null)
+  const setChat = (idChat: string, idUser: string, name: string, avatar: string) => {
+    setChatId(idChat)
+    setCurrentUserChat({ idUser: idUser, name: name, avatar: avatar })
+    onOpenChange(); setModalName("")
+  }
+  useEffect(() => {friend && setDataFriend(friend)},[friend])
   return <Modal
-  /* isOpen={isOpen} */
-  /* onOpenChange={() => { onOpenChange() }} */
->
-  <ModalContent>
-    {(onClose) => (
-      <>
-        <ModalHeader className="flex flex-col gap-1">Search</ModalHeader>
-        <ModalBody>
-          
-        </ModalBody>
-        <ModalFooter>
-          <Button color="danger" variant="light" onPress={() => { onClose() }}>
-            Close
-          </Button>
-        </ModalFooter>
-      </>
-    )}
-  </ModalContent>
-</Modal>
+    isOpen={isOpen}
+    onOpenChange={() => { onOpenChange(); setModalName("") }}
+  >
+    <ModalContent>
+      {(onClose) => (
+        <>
+          <ModalHeader className="flex flex-col gap-1">Friend list</ModalHeader>
+          <ModalBody>
+            {dataFriend?.map((f: any) =>
+              <div className="flex items-center justify-around" key={`friend-${f.name}`}>
+                <Avatar isBordered={f.online} color="success" radius="sm" src={f.avatar} />
+                <Code className="w-3/4 h-full flex items-center" radius="sm">{f.name}</Code>
+                <Button size="sm" color="primary" isIconOnly onClick={() => {setChat(`chat-${f.idFriend}`,f.idUser,f.name,f.avatar)}}>
+                  <IoChatboxEllipsesOutline />
+                  </Button>
+              </div>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" variant="light" onPress={() => { onClose(); setModalName("") }}>
+              Close
+            </Button>
+          </ModalFooter>
+        </>
+      )}
+    </ModalContent>
+  </Modal>
 
 }
 
