3d70422b9a8e4ebff751e310365dde97602fb202
dphasst17 <phathuu12@gmail.com>
2024-04-11 08:09:30 +0700

    update: add return data in socket message

--- server/src/utils/socket.ts
+++ server/src/utils/socket.ts
@@ -2,7 +2,7 @@ export const socketUserStatus = (io:any,status:string,idUser:string) => {
     return io.emit(status,idUser)
 }
 export const socketMessage = (io:any,data:any) => {
-    return io.emit('/message')
+    return io.emit('/message',data)
 }
 export const socketNoti = (io:any,data:any) => {
     return io.emit('friend',data)
