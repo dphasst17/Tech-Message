80f18ee2e953f39d22a8f4484c54b576ed15a4f6
dphasst17 <phathuu12@gmail.com>
2024-04-12 15:49:27 +0700

    fixed: fixed error ui in screen mobile

--- client/src/pages/home/chat/chatContent.tsx
+++ client/src/pages/home/chat/chatContent.tsx
@@ -1,18 +1,18 @@
-import { Avatar, Button, Code, Input, ScrollShadow } from "@nextui-org/react"
+import { Avatar, Button, Code, Input, ScrollShadow,PopoverContent, Popover, PopoverTrigger } from "@nextui-org/react"
 import MessageUi from "../../../components/ui/message"
 import { useContext, useEffect, useRef, useState } from "react"
 import { MdOutlineInsertEmoticon } from "react-icons/md";
 import { FaFileMedical } from "react-icons/fa";
+import { IoPersonAddOutline } from "react-icons/io5";
 import { IoImages } from "react-icons/io5";
 import { IoIosSend } from "react-icons/io";
-/* import { RxOpenInNewWindow } from "react-icons/rx"; */
 import { getChatDetail, sendMessage } from "../../../api/chatApi";
 import { StateContext } from "../../../context/stateContext";
 import { CiSquareInfo, CiSearch, CiPhone } from "react-icons/ci";
 import { GetToken } from "../../../utils/token";
 import { io } from "socket.io-client";
 const ChatContent = () => {
-    const { toggleNav,chatId, user, currentUserChat } = useContext(StateContext)
+    const { toggleNav, chatId, user, currentUserChat } = useContext(StateContext)
     const [dataMess, setDataMess] = useState<any[]>([])
     const [inputValue, setInputValue] = useState('')
     const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -90,8 +90,8 @@ const ChatContent = () => {
             message: inputValue,
             time: time,
             timestamp: date,
-            name:user[0]?.name,
-            avatar:user[0]?.avatar
+            name: user[0]?.name,
+            avatar: user[0]?.avatar
         })
             .then(res => {
                 if (res.status === 201) {
@@ -105,16 +105,38 @@ const ChatContent = () => {
     }
     return <>
         <Button className="md:hidden m-1" radius="sm" isIconOnly onClick={toggleNav}>
-            <Avatar src={user[0]?.avatar} radius="sm"/>
+            <Avatar src={user?.map((u: any) => u.avatar).toString()} radius="sm" />
         </Button>
         <div className="chatContent w-full md:w-[70%] h-[93%] md:h-full flex flex-wrap justify-center content-around bg-zinc-800 border border-solid border-zinc-400 rounded-lg p-1">
             {chatId !== "" && <div className="w-full h-[8%] flex items-center p-1 ">
                 <Avatar radius="lg" className="w-1/5 sm:w-[10%] !h-full" src={currentUserChat.avatar !== "" ? currentUserChat.avatar : ""} />
                 <Code className="w-3/4 sm:w-[65%] xl:w-[75%] h-full flex items-center p-2 mx-2 text-[20px] font-bold bg-zinc-900">{currentUserChat.name}</Code>
-                <div className="w-0 sm:w-1/4 xl:w-[15%] h-full flex items-center justify-around bg-zinc-900 rounded-lg">
-                    <CiSearch className="w-1/4 h-2/4" />
-                    <CiPhone className="w-1/4 h-2/4 -scale-x-100" />
-                    <CiSquareInfo className="w-1/4 h-2/4" />
+                <div className="w-0 sm:w-1/4 xl:w-[15%] h-full flex items-center justify-around bg-zinc-900 rounded-lg overflow-hidden">
+                    <Button size="sm" radius="sm" isIconOnly className="w-1/4 h-3/4 bg-transparent ">
+                        <CiSearch className="w-3/5 h-3/5" />
+                    </Button>
+
+                    <Button size="sm" radius="sm" isIconOnly className="w-1/4 h-3/4 bg-transparent ">
+                        <CiPhone className="w-3/5 h-3/5 -scale-x-100" />
+                    </Button>
+
+                    <Popover backdrop="opaque" radius="sm">
+                        <PopoverTrigger>
+                            <Button size="sm" radius="sm" isIconOnly className="w-1/4 h-3/4 bg-transparent ">
+                                <CiSquareInfo className="w-3/5 h-3/5" />
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[250px] min-h-[100px] flex justify-center items-start p-4">
+                            <Avatar className="mx-auto" size="lg" radius="sm"
+                            classNames={{base:['min-w-[100px] min-h-[80px]']}}
+                               src={currentUserChat.avatar !== "" ? currentUserChat.avatar : ""}
+                            />
+                            <Code className="w-full flex items-center justify-center my-1 cursor-pointer" radius="sm">
+                                {currentUserChat.name}
+                            </Code>
+                            {chatId.split("-")[1] === "group" && <Button size="sm" radius="sm" isIconOnly color="primary"><IoPersonAddOutline /></Button>}
+                        </PopoverContent>
+                    </Popover>
                 </div>
             </div>}
             <ScrollShadow className="w-full h-[80%] overflow-y-auto">
@@ -123,7 +145,7 @@ const ChatContent = () => {
                     {d.data.map((e: any) => <>
                         <MessageUi className="w-auto h-auto overflow-hidden my-4 flex items-center"
                             color={e.sender === user[0].idUser ? 'primary' : 'danger'} isTruncate={true} reverse={e.sender === user[0].idUser ? true : false}
-                            src={e.sender === user[0].idUser ? user[0].avatar : (e.avatar||currentUserChat.avatar)} content={e.message} height="auto" width="auto" />
+                            src={e.sender === user[0].idUser ? user[0].avatar : (e.avatar || currentUserChat.avatar)} content={e.message} height="auto" width="auto" />
                     </>)}
                 </>)}
                 <div ref={messagesEndRef} />
